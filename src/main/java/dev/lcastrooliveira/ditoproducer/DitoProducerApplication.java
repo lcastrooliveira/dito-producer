5c4a803161a6130091f835571af1415b0ddad0a5
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-02-21 20:41:39 -0300

    Commit inicial

---
+++ src/main/java/dev/lcastrooliveira/ditoproducer/DitoProducerApplication.java
@@ -0,0 +1,13 @@
+package dev.lcastrooliveira.ditoproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitoProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DitoProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lcastrooliveira/ditoproducer/DitoProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lcastrooliveira.ditoproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitoProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
